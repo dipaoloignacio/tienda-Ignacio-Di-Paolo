2c93a69944bc80196237800fea9a1b47d92f33dc
Ignacio Di Paolo <nachobeatbox@gmail.com>
2022-07-27 23:00:25 -0300

    created Navbar

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+      <nav className="Navbar">
+        <ul>
+          <li>Home</li>
+          <li>Market</li>
+          <li>Contact</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
